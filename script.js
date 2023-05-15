ba263f6d24ac764253a196b39b6ed62a7613e1f1
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-05-15 17:59:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 //your code here
 let priceList=document.querySelectorAll(".price")
-
+let table = document.querySelector("table");
 function func(){
 	let sum=0;
 	priceList.forEach((e)=>{
-		sum=sum+e.value
+		sum += +e.innerText
 	})
+	let tr= document.createElement("tr");
+    let td1=document.createElement("td");
+    td1.innerText="Total";
+	let td2=document.createElement("td");
+    td2.innerText=sum;
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	table.appendChild(tr);
+
 }
+
+func()
